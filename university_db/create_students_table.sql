1bfe4828aee0e4a825c8218741b0f142950bde82
Rendola14 <rendolaprincess_bsit@plmun.edu.ph>
2025-02-28 17:45:33 +0800

    Submitting ADVDBMS Week 02 - Session 01 - Exercise 02

--- university_db/create_students_table.sql
+++ university_db/create_students_table.sql
@@ -1,5 +1,3 @@
-Use `UniversityDB`;
-
 -- Step 1: Create the Students table with constraints
 CREATE TABLE `Students` (
    `StudentID` INT PRIMARY KEY AUTO_INCREMENT,  -- MySQL Example: AUTO_INCREMENT
